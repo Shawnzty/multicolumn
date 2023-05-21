22d826654360ada55309b0692bddd4f20279f485
Tianyi Zheng <zheng@neuron.t.u-tokyo.ac.jp>
2023-05-21 14:47:16 +0900

    update

--- funcs/getEnvelope.m
+++ funcs/getEnvelope.m
@@ -1,13 +1,29 @@
 function env = getEnvelope(signal)
 %GETENVELOPE get the peak of signal and concatenate them
 % signal is [16,time,5]
+% directly use matlab envelope may be better?
 env = zeros(16,size(signal,2),5);
+stimIn = 100000;
 for pop = 1:16
 for cond = 1:5
     [pks,locs] = findpeaks(signal(pop,:,cond));
-    xq = 1:size(signal,2);
-    vq = interp1(locs, pks, xq);
-    env(pop,:,cond) = vq;
+    xq_beforeStim = 1:stimIn;
+    xq_afterStim = (stimIn+1):size(signal,2);
+
+    before_locs = locs(locs<stimIn);
+    before_pks = pks(locs<stimIn);
+    after_locs = locs(locs>stimIn);
+    after_pks = pks(locs>stimIn);
+    
+    before_locs = [before_locs, stimIn];
+    before_pks = [before_pks, before_pks(end)];
+    after_locs = [stimIn+1, after_locs];
+    after_pks = [before_pks(end), after_pks];
+
+    vq_beforeStim = interp1(before_locs, before_pks, xq_beforeStim);
+    vq_afterStim = interp1(after_locs, after_pks, xq_afterStim);
+    tmp = cat(2, vq_beforeStim, vq_afterStim);
+    env(pop,:,cond) = tmp;
 end
 end
 
