01ec3ba821155c93d52891fe162d67928aec515e
Tianyi Zheng <zheng@neuron.t.u-tokyo.ac.jp>
2023-05-23 12:21:17 +0900

    update

---
+++ funcs/normTo1.m
@@ -0,0 +1,10 @@
+function outputArray = normTo1(inputArray)
+    % Ensure the input is a numeric array
+    assert(isnumeric(inputArray), 'Input must be a numeric array');
+    
+    % Compute the sum of the input array
+    totalSum = sum(inputArray(:));
+    
+    % Normalize the array so its elements add up to 1
+    outputArray = inputArray / totalSum;
+end
