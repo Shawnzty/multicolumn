a844417a4b442dabc4dda87db1d50a5f4b2a86b9
Tianyi Zheng <zheng@neuron.t.u-tokyo.ac.jp>
2023-06-05 18:04:50 +0900

    update

--- funcs/getPeriod.m
+++ funcs/getPeriod.m
@@ -1,7 +1,33 @@
-function [outputArg1,outputArg2] = getPeriod(inputArg1,inputArg2)
-%GETPERIOD Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
+function [period_begin, period_finish] = getPeriod(r)
+    % Input dimensions
+    [nChannels, nTime, nCases] = size(r);
 
+    % Filter parameters
+    fs = 100000; % sampling rate
+    fl = 20; % lower bound frequency
+    fh = 40; % upper bound frequency
+
+    % Initialize output
+    period_begin = zeros(nChannels, nCases);
+    period_finish = zeros(nChannels, nCases);
+
+    % Design bandpass filter
+    bpFilt = designfilt('bandpassiir','FilterOrder',20, ...
+        'HalfPowerFrequency1',fl,'HalfPowerFrequency2',fh, ...
+        'SampleRate',fs);
+
+    % Loop over channels and cases
+    for i = 1:nChannels
+        for j = 1:nCases
+            % Filter signal
+            filteredSignal = filtfilt(bpFilt, r(i,:,j));
+            % Find peaks
+            [~,locs] = findpeaks(filteredSignal);
+            % Get last period
+            if numel(locs) >= 2
+                period_begin(i,j) = locs(end-1);
+                period_finish(i,j) = locs(end);
+            end
+        end
+    end
+end
--- study/test.m
+++ study/test.m
@@ -3,8 +3,4 @@ strt_prd = 350001; end_prd = 400000;
 
 [r, v, g] = once(0.2, 0.02, 0.02, 4000);
 
-[powerR, centerfreqR, powerIntR] = getPowerFreq(r, strt_prd, end_prd, 20, 40);
-
-integralRC = squeeze(sum(r(:,strt_prd:end_prd,:),2));
-integralR0 = squeeze(sum(r(:,50001:100000,:),2));
-integralR = cat(2,integralR0(:,1), integralRC);
+[period_begin, period_finish] = getPeriod(r);
